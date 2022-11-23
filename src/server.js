dcfed6d52191d6f722936306f8f27894386dea2b
Reinaldo Vombo <reinaldo.vombo@gmail.com>
2022-11-23 22:24:13 +0100

    add start command

--- src/server.js
+++ src/server.js
@@ -85,4 +85,5 @@ app.get('/games/:id/discord', (request, response) => __awaiter(void 0, void 0, v
         discord: ad.discord
     });
 }));
-app.listen(process.env.port || 3333, () => console.log('server is runing'));
+const port = process.env.Port || 3333;
+app.listen(port, () => console.log(`server Running on port ${port}`));
--- src/utils/convert-hour-string-to-minutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.convertHourStringToMinutes = void 0;
-function convertHourStringToMinutes(hourString) {
-    const [hour, minutes] = hourString.split(':').map(Number);
-    const minutesAmount = hour * 60 + minutes;
-    return minutesAmount;
-}
-exports.convertHourStringToMinutes = convertHourStringToMinutes;
